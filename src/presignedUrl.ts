720ddb1d6ff101c518f28665e426cd13879adb90
Swizec Teller <swizec@swizec.com>
2021-04-20 08:00:26 -0700

    Presigned urls with specific type

--- src/presignedUrl.ts
+++ src/presignedUrl.ts
@@ -1,25 +1,45 @@
 import { v4 as uuidv4 } from "uuid";
 import { S3 } from "aws-sdk";
+import { APIGatewayEvent } from "aws-lambda";
 
 const s3 = new S3({
     apiVersion: "2006-03-01",
 });
 
-export async function getUrl() {
+export async function getUrl(event: APIGatewayEvent) {
+    const fileType = event.queryStringParameters?.filetype;
+
+    if (!fileType?.startsWith("image")) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ error: "Please upload an image" }),
+        };
+    }
+
+    // image/png -> png
+    const extension = fileType.split("/")[1];
+
     const imageId = uuidv4();
-    const filename = `${imageId}`;
+    const filename = `${imageId}.${extension}`;
     const expireSeconds = 60 * 5;
     const readUrl = `https://lovebox-stash.s3.amazonaws.com/${filename}`;
 
     const uploadUrl = s3.getSignedUrl("putObject", {
         Bucket: "lovebox-stash",
         Key: filename,
-        ContentType: "image/*",
+        ContentType: fileType,
         Expires: expireSeconds,
     });
 
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "access-control-allow-methods": "GET",
+    };
+
     return {
         statusCode: 200,
+        headers,
         body: JSON.stringify({
             uploadUrl,
             readUrl,
